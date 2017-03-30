25a82c359bbf2b72adae09d9c8965efce405823b
alexp <alexp@diatomenterprises.com>
2017-03-30 19:06:14 +0300

    Simple revers proxy.

--- src/ReversProxy/src/app.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello world111222');
-//# sourceMappingURL=app.js.map
--- src/ReversProxy/src/app.ts
+++ src/ReversProxy/src/app.ts
@@ -1,1 +1,18 @@
-﻿console.log('Hello world111222');
+﻿import * as http from "http";
+
+let httpProxy: any = require("http-proxy");
+
+
+let proxy: any = httpProxy.createProxyServer();
+
+http.createServer((req, res) => {
+    proxy.web(req, res, {
+        target: "http://localhost:6002"
+    });
+}).listen(6001);
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write(`request successfully proxied to: ${req.url}\n${JSON.stringify(req.headers)}`);
+    res.end();
+}).listen(6002);
